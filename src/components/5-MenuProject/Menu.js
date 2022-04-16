62f3f62402493f5d4d86eb512b62d7e24141fbba
Bryan Loh <bryan_2u@hotmail.com>
2022-04-16 20:01:53 +0800

    commit

--- src/components/5-MenuProject/Menu.js
+++ src/components/5-MenuProject/Menu.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuItems from "./MenuItems";
+import items from "./data";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+const allCategories = [
+  "all",
+  ...new Set(
+    items.map((item) => {
+      return item.category;
+    })
+  ),
+];
 
 const Menu = () => {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const key = { key: "value" };
+
   return (
     <section className="container">
-      <h2>5. Menu Project</h2>
+      <div>
+        <h2>5. Menu Project</h2>
+      </div>
+      <MenuItems items={menuItems} test={key} />
     </section>
   );
 };
---
+++ src/components/5-MenuProject/MenuItems.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MenuItems = (props) => {
+  const { items, key } = props;
+  return <div>{}</div>;
+};
+
+export default MenuItems;
